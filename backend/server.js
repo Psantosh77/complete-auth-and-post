7d94d882fe2a03c95d6811cd2e309d0c9386e3df
Santosh Pandit <Psantosh77>
2022-03-05 11:53:53 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const notes = require("./data/notes")
-const dotenv = require("dotenv")
-
+const dotenv = require("dotenv");
+const connectDB = require("./Config/db");
 const app = express();
+const userRoutes = require('./routes/userRoutes')
+
 dotenv.config();
+connectDB();
 
 app.get("/", (req,res)=>{
     res.send("Api is running")
@@ -17,12 +20,14 @@ app.get("/api/notes" , (req,res)=>{
 
 })
 
+app.use('/api/user', userRoutes)
 
-app.get("/api/notes/:id" , (req , res)=>{
-    const note = notes.find((n)=>n._id===req.params.id)
-    console.log(req.params)
-    res.send(note);
-});
+
+// app.get("/api/notes/:id" , (req , res)=>{
+//     const note = notes.find((n)=>n._id===req.params.id)
+//     console.log(req.params)
+//     res.send(note);
+// });
 
 PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
